849716304c6f2026db27ec10535bfde41caa9c84
Deepak Singh <deepaksingh8920984567@gmail.com>
2025-07-18 17:53:00 +0530

    Adding new files

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listings.js");  // Be carefull use single dot if it is outside only one directory.
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 
 // It is the code for starting our server. 
@@ -22,26 +27,66 @@ async function main(){
 
 
 
-// Setting our API's
+// Setting our API's Route
 app.get("/", (req,res)=>{
     res.send("Deepak is working on the Airbnb clone project");  // It is lsiteing at when call it at localhost:8080
 });
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
-// Setting API for testing listing
-app.get("/testListing", async (req,res)=>{
-let sampleListing = new Listing({
-    tittle : "my Villa",
-    description : "I is too much beatiful",
-    image : "https://unsplash.com/s/photos/hotel",
-    price : 1200,
-    location : "Goa",
-    country : "India", 
+// Index Route
+app.get("/listings", async(req,res)=>{
+const allListings = await Listing.find({});
+res.render("listings/index.ejs",{allListings});
 });
-await sampleListing.save();
-console.log("sample was saved");
-res.send("successful testing is done");
+
+// New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs"); 
 });
+
+// Show Route
+
+app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+const listing = await Listing.findById(id);
+res.render("listings/show.ejs",{listing});
+})
+
+// Create Route
+app.post("/listings", async(req,res)=>{
+    console.log(req.body);
+const newListing = new Listing(req.body.listing);
+await newListing.save();
+res.redirect("/listings");
+});
+
+
+// Edit Route
+app.get("/listings/:id/edit", async(req,res)=>{
+let {id} = req.params;
+const listing = await Listing.findById(id);
+res.render("listings/edit.ejs",{listing});
+});
+
+
+// update Route
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+
+app.delete("/listings/:id",async(req,res)=>{
+let {id} = req.params;
+let deletedListing = await Listing.findByIdAndDelete(id);
+console.log(deletedListing);
+res.redirect("/listings");
+});
+
 
 
 // It is used for running the server
