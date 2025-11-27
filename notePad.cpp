7fcf040814180efcd67f57f70408ae8f1c3b2738
Deepak Singh <deepaksingh8920984567@gmail.com>
2025-11-27 16:10:24 +0530

    commiting notePad.cpp file for deleting this file from the github

---
+++ notePad.cpp
@@ -0,0 +1,21 @@
+Q. Which command is used for opening the interface for the mongodb?
+ANS. mongosh
+
+Q. Which command is used for starting the project?
+ANS. nodemon app.js
+// Note --> app.js is main file name where all the routing is defined.
+
+Q. What are collections in mongodb?
+ANS. Collections are similar as the table in SQL.
+
+Q. Which command is used for see the data inside a particular collection?
+ANS. db.collectionName.find()
+
+Q. What is the differece between the use of class and className?
+ANS class is used in js,ejs while className is used in  React.
+
+Q. what is the name of the file of this project that is aquiring the navbar and footer?
+ANS. views/layouts/boilerplate.ejs
+
+Q. Which command is used for creating the space between two words?
+ANS. &nbsp;
